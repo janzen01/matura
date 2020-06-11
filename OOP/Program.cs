48e111bb418a5639b4a10dc7a57912069640df9a
Luboš Jánský <lubos.jansky@outlook.com>
2020-06-11 12:38:01 +0200

    added OOP

---
+++ OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zdravic zdravic = new Zdravic();
+            zdravic.text = "Ahoj uživateli";
+            Console.WriteLine(zdravic.Pozdrav("Karel"));
+            Console.WriteLine(zdravic.Pozdrav("Luboš"));
+            zdravic.text = "Vítej programátore";
+            Console.WriteLine(zdravic.Pozdrav("Davide"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/Zdravic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Zdravic
+    {
+        public string text;
+        
+        public string Pozdrav(string jmeno)
+        {
+            return string.Format("{0} {1}", text, jmeno);
+        }
+    }
+}
